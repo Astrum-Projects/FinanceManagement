922f421b0cf37c74b4293772a385149a2595d3d6
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-09-28 17:32:30 +0500

    UserRepository

---
+++ src/Infrastructure/Repositories/Users/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Users
+{
+    public interface IUserRepository
+    {
+        public Task<List<User>> GetAllUsers();
+        public Task<User> GetUserById(int id);
+        public Task<User> GetUserById(long telegramId);
+        public Task<User> CreateUser(User user);
+        public Task<User> DeleteUser(int id);
+    }
+}
---
+++ src/Infrastructure/Repositories/Users/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Repositories.Users
+{
+    internal class UserRepository : IUserRepository
+    {
+        public Task<User> CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserById(long telegramId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
