5d1be3a1a16a9b8a7653b71087c4dc811f6137dc
SardorSohinazarov <sardorstudent0618@gmail.com>
2024-09-28 17:32:38 +0500

    Nimadur

---
+++ src/Infrastructure/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transfer> Transfers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configuration.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ src/Infrastructure/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure
+{
+    public static class Configuration
+    {
+        public static string ConnectionString => "Host=127.0.0.1;Username=postgres;Password=Sardor0618!;Database=FinanceManagamentDb";
+    }
+}
